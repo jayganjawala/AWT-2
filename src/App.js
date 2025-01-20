3c15da09e944a165b806707705c0d0c1a37f9295
jayganjawala <jayganjawala.it22@scet.ac.in>
2025-01-20 14:15:48 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import UserProfile from './UserProfile';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const details = {
+    name : "Jay Ganjawala",
+    age : 20,
+    location : "Surat",
+    bio : "Har Har Mahadev❤️"
+  }
+
+  return(
+    <UserProfile  name={details.name} age={details.age} loc={details.location} bio={details.bio}/>
+  )
 }
 
 export default App;
